d6fa27019e133a878f1a10c55f145af659ce7bc9
Matthew Barbour <sanmadjack@gmail.com>
2012-08-16 09:44:52 -0500

    Added new SimpleModel so that I can use the methods I've added to observable collection in more generic models

--- Model.cs
+++ Model.cs
@@ -7,20 +7,12 @@ using System.Linq;
 using System.Windows.Threading;
 namespace MVC {
 
-    public class Model<T> : Model<StringID, T> where T : AModelItem<StringID> {
-    }
-
-    public class Model<I, T> : ObservableCollection<T>
+    public class Model<I, T> : SimpleModel<T>
         where T : AModelItem<I>
         where I : AIdentifier {
 
-        public new IList<T> Items {
-            get {
-                return base.Items;
-            }
-        }
 
-        public Boolean containsId(I id) {
+                public Boolean containsId(I id) {
             foreach (T item in this) {
                 if (item.id.Equals(id))
                     return true;
@@ -36,90 +28,10 @@ namespace MVC {
             return null;
         }
 
-        protected bool _cancelling = false;
-        public void cancel() { _cancelling = true; }
-
-
-
-        public new event PropertyChangedEventHandler PropertyChanged;
-        public void NotifyPropertyChanged(string prop) {
-            OnPropertyChanged(new PropertyChangedEventArgs(prop));
-        }
-
-        public void AddRange(ICollection<T> us) {
-            foreach (T me in us) {
-                AddWithSort(me);
-            }
-
-        }
-
-        public void AddWithSort(T add_me) {
+        public override void AddWithSort(T add_me) {
             add_me.PropertyChanged += new PropertyChangedEventHandler(NotifyItemPropertyItemChanged);
-            if (this.Count == 0) {
-                this.InsertItem(0, add_me);
-            } else {
-                for (int i = 0; i < this.Count; i++) {
-                    T compare = this[i];
-                    if (compare.CompareTo(add_me) > 0) {
-                        this.InsertItem(i, add_me);
-                        return;
-                    }
-                }
-                this.InsertItem(this.Count, add_me);
-            }
+            base.AddWithSort(add_me);
         }
-
-        public event PropertyChangedEventHandler ItemPropertyChanged;
-        void NotifyItemPropertyItemChanged(object sender, PropertyChangedEventArgs e) {
-            if (ItemPropertyChanged != null) {
-                ItemPropertyChanged(sender, e);
-            }
-        }
-
-
-        private bool _isEnabled = true;
-        public bool IsEnabled {
-            get { return _isEnabled; }
-            set {
-                if (value != _isEnabled) {
-                    _isEnabled = value;
-                    this.NotifyPropertyChanged("IsEnabled");
-                }
-
-            }
-        }
-
-        public Model() {
-        }
-
-        public virtual void Refresh() {
-            NotifyCollectionChangedEventArgs e =
-                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
-            try {
-                this.OnCollectionChanged(e);
-            } catch (Exception ex) {
-                throw new Exception("Error While Refreshing List", ex);
-            }
-        }
-
-        public override event NotifyCollectionChangedEventHandler CollectionChanged;
-        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e) {
-            var eh = CollectionChanged;
-            if (eh != null) {
-                Dispatcher dispatcher = (from NotifyCollectionChangedEventHandler nh in eh.GetInvocationList()
-                                         let dpo = nh.Target as DispatcherObject
-                                         where dpo != null
-                                         select dpo.Dispatcher).FirstOrDefault();
-
-                if (dispatcher != null && dispatcher.CheckAccess() == false) {
-                    dispatcher.Invoke(DispatcherPriority.DataBind, (Action)(() => OnCollectionChanged(e)));
-                } else {
-                    foreach (NotifyCollectionChangedEventHandler nh in eh.GetInvocationList())
-                        nh.Invoke(this, e);
-                }
-            }
-        }
-
 
         public List<T> SelectedItems {
             get {
@@ -133,4 +45,9 @@ namespace MVC {
         }
 
     }
+
+    public class Model<T> : Model<StringID, T> where T : AModelItem<StringID> {
+    }
+
+
 }
---
+++ SimpleModel.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Threading;
+namespace MVC {
+    public class SimpleModel<T> : ObservableCollection<T> {
+
+        public new IList<T> Items {
+            get {
+                return base.Items;
+            }
+        }
+
+
+        protected bool _cancelling = false;
+        public void cancel() { _cancelling = true; }
+
+
+        public void AddRange(ICollection<T> us) {
+            foreach (T me in us) {
+                AddWithSort(me);
+            }
+
+        }
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string prop) {
+            OnPropertyChanged(new PropertyChangedEventArgs(prop));
+        }
+
+        public virtual void AddWithSort(T add_me) {
+            if (this.Count == 0) {
+                this.InsertItem(0, add_me);
+            } else {
+                for (int i = 0; i < this.Count; i++) {
+                    T compare = this[i];
+                    int result = 0;
+                    if (compare is IComparable) {
+                        IComparable comparer = compare as IComparable;
+                        result = comparer.CompareTo(add_me);
+                    } else if (compare is IComparable<T>) {
+                        IComparable<T> comparerr = compare as IComparable<T>;
+                        result = comparerr.CompareTo(add_me);
+                    } else {
+                        compare.ToString().CompareTo(add_me.ToString());
+                    }
+                    if (result > 0) {
+                        this.InsertItem(i, add_me);
+                        return;
+                    }
+                }
+                this.InsertItem(this.Count, add_me);
+            }
+        }
+
+        public event PropertyChangedEventHandler ItemPropertyChanged;
+        protected void NotifyItemPropertyItemChanged(object sender, PropertyChangedEventArgs e) {
+            if (ItemPropertyChanged != null) {
+                ItemPropertyChanged(sender, e);
+            }
+        }
+
+
+        private bool _isEnabled = true;
+        public bool IsEnabled {
+            get { return _isEnabled; }
+            set {
+                if (value != _isEnabled) {
+                    _isEnabled = value;
+                    this.NotifyPropertyChanged("IsEnabled");
+                }
+
+            }
+        }
+
+        public SimpleModel() {
+        }
+
+        public virtual void Refresh() {
+            NotifyCollectionChangedEventArgs e =
+                new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset);
+            try {
+                this.OnCollectionChanged(e);
+            } catch (Exception ex) {
+                throw new Exception("Error While Refreshing List", ex);
+            }
+        }
+
+        public override event NotifyCollectionChangedEventHandler CollectionChanged;
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e) {
+            var eh = CollectionChanged;
+            if (eh != null) {
+                Dispatcher dispatcher = (from NotifyCollectionChangedEventHandler nh in eh.GetInvocationList()
+                                         let dpo = nh.Target as DispatcherObject
+                                         where dpo != null
+                                         select dpo.Dispatcher).FirstOrDefault();
+
+                if (dispatcher != null && dispatcher.CheckAccess() == false) {
+                    dispatcher.Invoke(DispatcherPriority.DataBind, (Action)(() => OnCollectionChanged(e)));
+                } else {
+                    foreach (NotifyCollectionChangedEventHandler nh in eh.GetInvocationList())
+                        nh.Invoke(this, e);
+                }
+            }
+        }
+
+
+
+    }
+}
